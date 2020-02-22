d6fd9f3d957cf05c013b76d1f0c7d4b6db4fe117
WakinDed <Labuskij1997@gmail.com>
2020-02-22 13:54:05 +0300

    Initial Commit

---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by Роман Лабунский on 22/02/2020.
+//  Copyright © 2020 Роман Лабунский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
